b805604c839043f520caf1451e0d9a8ce9a21cdd
Chao Ma <machaosydney@gmail.com>
2023-07-14 13:55:06 +1000

    feat: Implement DarkModeToggle component

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import Navbar from '@/components/Navbar/Navbar';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Footer from '@/components/Footer/Footer';
-
+import { ThemeProvider } from '@/context/ThemeContext';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -14,11 +14,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <div className='container'>
-          <Navbar />
-          {children}
-          <Footer />
-        </div>
+        <ThemeProvider>
+          <div className='container'>
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
