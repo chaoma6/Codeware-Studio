a20c6d4eba2f5f5f38468df52b1c32a6a2761d02
Chao Ma <machaosydney@gmail.com>
2023-07-14 15:02:19 +1000

    feat: add portfolio page

---
+++ src/app/portfolio/[category]/data.js
@@ -0,0 +1,71 @@
+export const items = {
+  application: [
+    {
+      id: 1,
+      title: 'Creative Portfolio',
+      desc: 'Build a stunning portfolio website to showcase your creative projects and attract potential clients. Highlight your skills, achievements, and previous work in an elegant and visually appealing manner.',
+      image:
+        'https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg',
+    },
+    {
+      id: 2,
+      title: 'Minimal Single Product',
+      desc: 'Create a minimalist and modern website for showcasing a single product. Emphasize the unique features and benefits of your product using clean design and attention-grabbing visuals to captivate potential customers.',
+      image:
+        'https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg',
+    },
+    {
+      id: 3,
+      title: 'Strong Together Charity',
+      desc: 'Establish an online platform for a charitable organization dedicated to making a positive impact in society. Share inspiring stories, encourage donations, and invite volunteers to join your noble cause.',
+      image:
+        'https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg',
+    },
+  ],
+  illustrations: [
+    {
+      id: 1,
+      title: 'Creative Portfolio',
+      desc: 'Showcase your artistic skills and creativity through a captivating portfolio of illustrations. Display a wide range of your artwork, from intricate sketches to vibrant digital designs, to attract clients and art enthusiasts.',
+      image:
+        'https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg',
+    },
+    {
+      id: 2,
+      title: 'Minimal Single Product',
+      desc: 'Present your unique product through stunning illustrations with a minimalist design approach. Capture attention with beautifully crafted visuals that highlight the key features and create an unforgettable impression.',
+      image:
+        'https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg',
+    },
+    {
+      id: 3,
+      title: 'Strong Together Charity',
+      desc: 'Convey the message of unity and support for a charitable cause through powerful illustrations. Use imagery that inspires compassion and showcases the positive impact of coming together to make a difference.',
+      image:
+        'https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg',
+    },
+  ],
+  websites: [
+    {
+      id: 1,
+      title: 'Creative Portfolio',
+      desc: 'Craft a visually stunning and user-friendly website to showcase your creative portfolio. Create an immersive browsing experience that highlights your skills, expertise, and unique approach to design.',
+      image:
+        'https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg',
+    },
+    {
+      id: 2,
+      title: 'Minimal Single Product',
+      desc: "Design a sleek and modern website to showcase a single product with elegance and simplicity. Emphasize the product's exceptional qualities and provide an intuitive browsing experience for potential customers.",
+      image:
+        'https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg',
+    },
+    {
+      id: 3,
+      title: 'Strong Together Charity',
+      desc: 'Develop an impactful website for a charity organization dedicated to making a positive change. Engage visitors with heartwarming stories, encourage donations, and inspire them to contribute to your noble mission.',
+      image:
+        'https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg',
+    },
+  ],
+};
