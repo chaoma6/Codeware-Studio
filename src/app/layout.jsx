63e138f7377223a89f3dcdba72f7c7c83fea5d5c
Chao Ma <machaosydney@gmail.com>
2023-07-15 16:27:40 +1000

    fix: layout style

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
-import Navbar from '@/components/Navbar/Navbar';
+import Navbar from '@/components/navbar/Navbar';
 import './globals.css';
 import { Inter } from 'next/font/google';
-import Footer from '@/components/Footer/Footer';
+import Footer from '@/components/footer/Footer';
 import { ThemeProvider } from '@/context/ThemeContext';
 import AuthProvider from '@/components/AuthProvider/AuthProvider';
 
